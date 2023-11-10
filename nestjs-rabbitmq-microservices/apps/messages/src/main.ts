0148c01fbfabe115fbf42954ba796f30f664dd4e
Soxomer <omarjetti@gmail.com>
2023-11-10 02:45:52 +0100

    auth + config + doc(debut) + jwt + pipe

---
+++ nestjs-rabbitmq-microservices/apps/messages/src/main.ts
@@ -0,0 +1,9 @@
+import {NestFactory} from '@nestjs/core';
+import {MessagesModule} from './messages.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  await app.listen(3000);
+}
+
+bootstrap();
