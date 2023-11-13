a695ce84f3b6c4d92cc33a7f57cca1428a246a89
RandomSkill <simon.sabot@Gmail.com>
2023-11-13 01:09:15 +0100

    wip: user.controller.spec test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -6,16 +6,29 @@ import { describe } from "node:test";
 describe("UsersController", () => {
   let controller: UsersController;
 
+  const mockUserService = {
+    findAll: jest.fn(),
+    findAllByAge: jest.fn(),
+    findOne: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("get /", () => {
+    expect(controller.findAll()).toHaveBeenCalled();
   });
 });
