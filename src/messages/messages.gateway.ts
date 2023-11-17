a061b8ff640c9a846b57271091d1c4ab26ffe265
guimochi <yamamoto.guilherme@outlook.com>
2023-11-17 18:50:50 +0100

    feature messages done

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -3,46 +3,39 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer,
-} from "@nestjs/websockets";
-import {MessagesService} from "./messages.service";
-import {CreateMessageDto} from "./dto/create-message.dto";
+  WebSocketServer
+} from '@nestjs/websockets';
+import {MessagesService} from './messages.service';
+import {CreateMessageDto} from './dto/create-message.dto';
 import {Server, Socket} from "socket.io";
+import {Message} from "./entities/message.entity";
 
-@WebSocketGateway({
-  cors: {
-    origin: "*",
-  },
-})
+@WebSocketGateway()
 export class MessagesGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(private readonly messagesService: MessagesService) {
+  }
 
-  @SubscribeMessage("createMessage")
-  async create(@MessageBody() createMessageDto: CreateMessageDto) {
-    const message = await this.messagesService.create(createMessageDto);
-    this.server.emit("newMessage", message);
+  @SubscribeMessage('createMessage')
+  async create(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    const message = this.messagesService.create(createMessageDto, client.id);
+    client.broadcast.emit('newMessage', message);
     return message;
   }
 
-  @SubscribeMessage("findAllMessages")
-  findAll() {
+  @SubscribeMessage('findAllMessages')
+  findAll(): Message[] {
     return this.messagesService.findAll();
   }
 
-  @SubscribeMessage("join")
-  join(@MessageBody("name") name: string, @ConnectedSocket() client: Socket) {
-    this.messagesService.join(name, client.id);
+  @SubscribeMessage('join')
+  join(
+      @MessageBody('name') name: string,
+      @ConnectedSocket() client: Socket
+  ): string[] {
+    return this.messagesService.join(name, client.id);
   }
 
-  @SubscribeMessage("typing")
-  typing(
-    @MessageBody("isTyping") isTyping: boolean,
-    @ConnectedSocket() client: Socket,
-  ) {
-    const name = this.messagesService.getClientName(client.id);
-    return client.broadcast.emit("typing", { name, isTyping });
-  }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,28 +1,34 @@
-import {Injectable} from "@nestjs/common";
-import {CreateMessageDto} from "./dto/create-message.dto";
+import {Injectable} from '@nestjs/common';
+import {CreateMessageDto} from './dto/create-message.dto';
 import {Message} from "./entities/message.entity";
 
 @Injectable()
 export class MessagesService {
-  messages: Message[] = [{ sender: "omar", content: "random texte" }];
-  clients = {};
+  messages: Message[] = [{sender: "omar", content: 'random texte'}];
+  clientToUser = {};
 
-  create(createMessageDto: CreateMessageDto) {
-    const message = { ...createMessageDto };
-    return this.messages.push(createMessageDto);
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message: Message = {
+      sender: this.clientToUser[clientId],
+      content: createMessageDto.content
+    };
+    this.messages.push(message);
+    return message;
   }
 
-  findAll() {
+  findAll(): Message[] {
     return this.messages;
   }
 
-  join(name: string, clientId: string) {
-    this.clients[clientId] = name;
-    return Object.values(this.clients);
+  join(name: string, clientId: string): string[] {
+    this.clientToUser[clientId] = name;
+    return Object.values(this.clientToUser);
   }
+
   getClientName(clientId: string) {
-    return this.clients[clientId];
+    return this.clientToUser[clientId];
   }
+
   typing() {
     // todo
   }
